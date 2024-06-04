2b5e3ab0dd9890b0306f581f8d6a48d025837934
Severin Fernando Tanga Mvondo <jarodak47@gmail.com>
2024-06-04 10:02:14 +0100

    Add new endpoint for listing of orders with pagination, and detail of an order,add buyer_uuid when creating buyer an order.

--- app/main/controllers/transaction_controller.py
+++ app/main/controllers/transaction_controller.py
@@ -1,11 +1,12 @@
 from fastapi import APIRouter
-from fastapi import Depends, HTTPException
+from fastapi import Depends, HTTPException,Query
 from sqlalchemy.orm import Session
 from typing import List
 from app.main import models, schemas
 from app.main.core.dependencies import get_db
 from app.main.crud.buyer_info import create_buyer
-from app.main.crud.order_crud import create_order_products, get_order_products
+from app.main.crud.order_crud import create_order_products, get_order_products, get_order_with_pagination,get_order_with_uuid
+
 
 router = APIRouter(prefix="", tags=["transaction"])
 
@@ -14,7 +15,7 @@ router = APIRouter(prefix="", tags=["transaction"])
 def save_buyer_information(buyer: schemas.BuyerCreate, token: str, db: Session = Depends(get_db)):
     try:
         db_article = create_buyer(db=db, obj_in=buyer, token=token)
-        return {"message": "Article created successfully", "article": db_article}
+        return db_article
     except HTTPException as e:
         raise e
     except Exception as e:
@@ -22,9 +23,9 @@ def save_buyer_information(buyer: schemas.BuyerCreate, token: str, db: Session =
 
 
 @router.post("/order/create/{token}")
-def creat_order(order: schemas.OrderCreate, token: str, db: Session = Depends(get_db)):
+def creat_order(order: schemas.OrderCreate, token: str,buyer_uuid:str, db: Session = Depends(get_db)):
     try:
-        db_article = create_order_products(db=db, obj_in=order, token=token)
+        db_article = create_order_products(db=db, obj_in=order, token=token, buyer_uuid = buyer_uuid)
         return {"message": "Article created successfully", "article": db_article}
     except HTTPException as e:
         raise e
@@ -41,6 +42,53 @@ def get_orders(token: str,
         orders = get_order_products(db=db, token=token, code=code)
 
         return orders
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{str(e)}")
+
+@router.get("/orders/get/{token}", response_model = schemas.DataList)
+def get_orders_with_pagination(
+       token: str,
+       order: str = Query("ASC", enum =["ASC", "DESC"]),
+       order_status : str = Query(None, enum =["PAID","PENDING","CANCELLED"]),
+        order_type : str = Query(None, enum =["SELLED", "BUYED"]),
+       page : int = 1,
+       per_page :int = 30,
+       db: Session = Depends(get_db),
+    ):
+    try:
+        orders = get_order_with_pagination(
+            db=db,
+            token=token,
+            page=page,
+            per_page=per_page,
+            order=order,
+            order_status=order_status,
+            order_type = order_type
+        )
+
+        return orders
+    except HTTPException as e:
+        raise e
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"{str(e)}")
+
+@router.get("/orders/get/{token}/{order_uuid}", response_model = schemas.OrderDetail)
+def get_order_by_uuid(
+       token: str,
+        order_uuid: str ,
+       db: Session = Depends(get_db),
+):
+
+    try:
+        order = get_order_with_uuid(
+            db=db,
+            token=token,
+            order_uuid = order_uuid
+        )
+
+        return order
     except HTTPException as e:
         raise e
     except Exception as e:
--- app/main/crud/order_crud.py
+++ app/main/crud/order_crud.py
@@ -9,11 +9,10 @@ from app.main.core.security import decode_access_token, generate_code
 from app.main.services import auth
 
 
-def create_order_products(db: Session, obj_in: schemas.OrderCreate, token: str):
+def create_order_products(db: Session, obj_in: schemas.OrderCreate, token: str,    buyer_uuid:str):
     valid_token = auth.get_auth_token(token=token)
-    print(f"................{valid_token['sub']}")
+    print("=======",obj_in.dict())
     if valid_token is not None:
-
         total_order_quantity = 0
         total_order_price = 0
         articles = []
@@ -24,7 +23,7 @@ def create_order_products(db: Session, obj_in: schemas.OrderCreate, token: str):
             total_order_quantity += product.quantity
             total_order_price += artile.price * product.quantity
         order = models.Order(uuid=str(uuid.uuid4()), user_uuid=valid_token['sub'], total_quantity=total_order_quantity,
-                             total_price=total_order_price, code=code)
+                             total_price=total_order_price, code=code, buyer_uuid=buyer_uuid)
         db.add(order)
         db.flush()
         index = 0
@@ -53,12 +52,81 @@ def get_order_products(
 ):
     valid_token = auth.get_auth_token(token=token)
     if valid_token is not None:
-        user = auth.get_user(token=token)
-
         order = db.query(models.Order).filter(models.Order.code == code).first()
+        user = auth.get_user(token=token,user_uuid= order.user_uuid)
+
         if not order:
             raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="this code is not valid")
 
         return {"order": order, "user": user}
 
     raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="token is not valid")
+
+def get_order_with_pagination(
+        db: Session,
+        token: str,
+        order: str = None,
+        order_type: str = None, # "selled", "buyed"
+        order_status: str = None,
+        page: int = 1,
+        per_page: int = 30
+):
+
+    valid_token = auth.get_auth_token(token=token)
+    orders =[]
+    if valid_token is not None:
+        orders = db.query(models.Order)
+
+        orders = db.query(models.Order).\
+            filter(
+                or_(
+                        models.Order.user_uuid == decode_access_token(token)['sub'],
+                        models.Order.buyer_uuid == decode_access_token(token)['sub'],
+                )
+        )
+        print(orders)
+        if not order:
+            order ="ASC"
+
+        if order_type and order_type == 'SELLED':
+            orders =orders.filter(models.Order.user_uuid == decode_access_token(token)['sub'])
+
+        if order_type and order_type == 'BUYED':
+            orders =orders.filter(models.Order.buyer_uuid == decode_access_token(token)['sub'])
+
+        if order_status and order_status == 'PAID':
+            orders =orders.filter(models.Order.status == "PAID")
+
+        if  order_status and order_status == 'CANCELLED':
+            orders =orders.filter(models.Order.status == "CANCELLED")
+
+        if order_status and order_status == 'PENDING':
+            orders =orders.filter(models.Order.status == "PENDING")
+
+        orders= orders.order_by(getattr(models.Order,"date_added" ,str(order).lower()))
+
+        total = orders.count()
+        orders =orders.offset((page - 1) * per_page).limit(per_page).all()
+
+        return schemas.DataList(
+             total = total,
+            pages=math.ceil(total / per_page),
+            current_page=page,
+            per_page=per_page,
+            data=orders
+        )
+
+def get_order_with_uuid(
+        db: Session,
+        token: str,
+        order_uuid: str,
+):
+    valid_token = auth.get_auth_token(token=token)
+    if valid_token is not None:
+        order = db.query(models.Order).filter(models.Order.uuid == order_uuid).first()
+        user = auth.get_user(token=token)
+        user_uuid = decode_access_token(token)[""]
+
+        if not order:
+            raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="order not found")
+        return order
